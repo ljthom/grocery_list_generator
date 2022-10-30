3e084fa90ffdb61fdb547ac576a0418511380ff1
Cameron <hayesraven@gmail.com>
2022-10-30 12:13:57 -0400

    Worked on conversions code more

--- pantry.py
+++ pantry.py
@@ -16,6 +16,7 @@ class ImpToMetConversions:
     def lbs_to_g(self, lbs):
         return lbs * 454
     
+    
 # Metric to imperial unit conversions
 class MetToImpConversions:
     # Convert milliliter to fluid ounce (or ounce)
@@ -24,4 +25,31 @@ class MetToImpConversions:
     
     # Convert grams to pounds
     def g_to_lbs(self, g):
-        return g / 454
+        return g / 454  
+
+# Big imperial to little imperial conversions
+# Mainly used to support imperial to metric conversions
+class ImpToImpConversions:
+    # Gallon to fluid ounce (or ounce)
+    def gal_to_floz(self, gal):
+        return gal * 128
+    
+    # Quart to fluid ounce (or ounce)
+    def qt_to_floz(self, qt):
+        return qt * 32
+    
+    # Pint to fluid ounce (or ounce)
+    def pt_to_floz(self, pt):
+        return pt * 16
+    
+    # Cup to fluid ounce (or ounce)
+    def cp_to_floz(self, cp):
+        return cp * 8
+    
+    # Tablespoon to fluid ounce (or ounce)
+    def tbsp_to_floz(self, tbsp):
+        return tbsp * 0.5
+    
+    # Teaspoon to fluid ounce (or ounce)
+    def tsp_to_floz(self, tsp):
+        return tsp * 0.166667
