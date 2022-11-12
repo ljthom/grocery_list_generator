53929fab1a01f97d83c97f0fc25f50972350583c
Vegtam <wanderingvegtam@gmail.com>
2022-11-11 23:18:34 -0500

    Added version requirements to install process

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 import setuptools
 
+with open('requirements.txt', encoding='utf-8') as reqs:
+    required = reqs.read().splitlines()
+
 setuptools.setup(
     name='pantry',
     packages=['pantry'],
     include_package_data=True,
-    install_requires=[
-        'flask',
-        'flask_sqlalchemy',
-        'flask-wtf',
-        'jsonschema',
-        'recipe-scrapers'
-    ],
+    install_requires=required
 )
