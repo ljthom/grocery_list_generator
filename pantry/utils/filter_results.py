de77fb4cfd618f986260ee93bf523ea2bd5f91de
Zachary Burson <44591160+zchbrsn@users.noreply.github.com>
2022-11-29 21:55:35 -0600

    Update filter_results.py
    Queries and parses DB data.  Need to add functions for user input to filter results.

--- pantry/utils/filter_results.py
+++ pantry/utils/filter_results.py
@@ -14,64 +14,58 @@ def connect():
             try:
                 global conn
                 conn = sqlite3.connect(args.file)
-                print(f"Connected to {args.file}")
             except sqlite3.Error as error:
                 print("Failed to insert data into table", error)
         else:
             print("Unable to locate DB")
 
 def query():
-    global cursor
-    global ingredients
+    global CURSOR
+    global INGREDIENTS
+    global TITLES
+    global URLS
+    CURSOR = conn.cursor()
+    TITLES = CURSOR.execute("SELECT title from recipes").fetchall()
+    INGREDIENTS = CURSOR.execute("SELECT title,ingredients from recipes").fetchall()
+    URLS = CURSOR.execute("SELECT title,url from recipes").fetchall()
+    IMAGES = CURSOR.execute("SELECT title,image from recipes").fetchall()
+
+def stripall(data):
     global results
-    global titles
-    global urls
-    global meta
-    cursor = conn.cursor()
-#    results = cursor.execute("SELECT ingredients FROM recipes")
-    ingredients = cursor.execute("SELECT ingredients from recipes").fetchall()
-    results = cursor.execute("SELECT title, ingredients from recipes").fetchall()
-    titles = cursor.execute("SELECT title from recipes").fetchall()
-    urls = cursor.execute("SELECT url from recipes").fetchall()
-#    meta = cursor.execute("SELECT ingredients, length(ingredients) from recipes ORDER BY length(ingredients) DESC").fetchall()
-#    results = cursor.execute("SELECT * from recipes WHERE recipes.ingredients like ?", (ingredients,))
+    results = []
+    for each in data:
+        results.append(list(filter(lambda item: item is not None, each)))
+    return results
 
-connect()
-query()
-recipes = []
-final = []
-for each in results:
-    final.append(list(filter(lambda item: item is not None, each)))
+def build(ingredients, titles):
+    a = stripall(ingredients)
+    b = stripall(titles)
+    c = {}
+    counter = 0
+    stored = ''
+    for each in a:
+        if each in b:
+            stored = str(each)
+            counter = 0
+        elif counter > 0:
+            c[stored].append(each)
+        else:
+            c.update({stored:[]})
+            counter += 1
+    return c
 
-#for each in final:
-#    print(each)
-#    while each not in titles:
-#        print(' '.join(each))
+def printall(recipes):
+    for key,value in recipes.items():
+        temp = re.search(r'.*?\'(.*)\'.*', key)
+        if temp is not None:
+            print(f"Title:\t\t{temp.group(1)}")
+            print(f"Ingredients:\t{value}")
+            print(f"URL:\t\t{CURSOR.execute('SELECT url FROM recipes WHERE title like ?', [temp.group(1)]).fetchall()[0][0]}")
+            print(f"Image:\t\t{CURSOR.execute('SELECT image FROM recipes WHERE title like ?', [temp.group(1)]).fetchall()[0][0]}")
+        print()
 
-for each in final:
-    if each in titles[1]:   # NEED TO EXPAND THE TUPLES FROM TITLES TO CHECK
-        print(each)
-
-#print(recipes)
-'''
-for each in titles:
-    if None not in each:
-        print(each)
-'''
-#res = list(filter([i for i in results if i is not None]))
-#res = list(filter(lambda item: item is not None, results))
-#recipes = ' '.join(filter(lambda item: item is not None, res))
-#print(recipes)
-
-#recipe = []
-#for each in res:
-    #print(each[2])
-#    recipes.append(each[2])
-#recipe = [x for x in recipes if x is not None]
-# THIS WORKS recipe = ' '.join(filter(lambda item: item is not None, recipes))
-
-# Query database using SELECT * FROM recipes WHERE INGREDIENTS=
-
-# Use regex to filter further
-
-# print results (for now)
+if __name__ == '__main__':
+    connect()
+    query()
+    recipes = build(INGREDIENTS, TITLES)
+    printall(recipes)
