20a18fb794bbb2f5a9ada8744a59f50b89b652ed
Zachary Burson <44591160+zchbrsn@users.noreply.github.com>
2022-12-05 19:24:54 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 import setuptools
 import zipfile
 
-with zipfile.ZipFile("./pantry/models/recipes.db.zip", 'r') as zip_ref:
-    zip_ref.extractall("./pantry/models/recipes.db")
-
 with open('requirements.txt', encoding='utf-8') as reqs:
     required = reqs.read().splitlines()
 
@@ -13,3 +10,6 @@ setuptools.setup(
     include_package_data=True,
     install_requires=required
 )
+
+with zipfile.ZipFile("./pantry/models/recipes.db.zip", 'r') as zip_ref:
+    zip_ref.extractall("./pantry/models/recipes.db")
